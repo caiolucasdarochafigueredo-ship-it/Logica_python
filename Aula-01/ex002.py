341773e196982d7b07ce887b7dedc8b0734291c2
caiolucasdarochafigueredo-ship-it <caiolucasdarochafigueredo@gmail.com>
2026-06-19 11:19:11 -0300

    feat(ex002): Adicionar aula 2

---
+++ Aula-01/ex002.py
@@ -0,0 +1,7 @@
+Caio=input("Qual seu nome?")
+print(f"Olá, {Nome}! Prazer em te conhecer.")
+Dia = input("Qual é o dia?")
+Mês = input("Qual é o mês?")
+Ano = input("Qual é o ano?")
+
+print(f"Voce nasceu no dia {Dia} do Mês de {Mês} no Ano {Ano}")
